966f6f6d4ba8f23e1df3afa38255ca1d3c646305
MightyTAL <tal2023@buaa.edu.cn>
2024-09-06 21:32:42 +0800

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function toggleContent(id) {
     var content = document.getElementById(id);
-    var toggleIcon = content.previousElementSibling.querySelector('.toggle-icon');
-    
+    var button = content.previousElementSibling;
+
     if (content.style.display === "none" || content.style.display === "") {
         content.style.display = "block";
-        toggleIcon.textContent = "[-]";
-        toggleIcon.classList.add('expanded');
+        button.textContent = "Read Less [-]";
     } else {
         content.style.display = "none";
-        toggleIcon.textContent = "[+]";
-        toggleIcon.classList.remove('expanded');
+        button.textContent = "Read More [+]";
     }
 }
